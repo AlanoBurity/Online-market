49afea52d12a77d4c83bee2d3e6b4458efcbf2be
AlanoBurity <alanosantosdev@gmail.com>
2022-07-09 16:28:39 -0300

    resolvento conflitos req 7

--- src/components/CardItem.jsx
+++ src/components/CardItem.jsx
@@ -4,33 +4,49 @@ import '../css/CardItem.css';
 import { Link } from 'react-router-dom';
 
 export default class CardItem extends Component {
+  // constructor() {
+  //   super();
+
+  //   this.state = {
+  //     getIt: '',
+  //   };
+  // }
+
+  // handleClick = (event) => {
+  //   this.focus()
+  //   }
+  // }
+  // focus = () => this.ref.focus;
+
   render() {
     const { id, title, price, thumbnail } = this.props;
     return (
       <Link to={ `/ItemPage/${id}` }>
-        <div
-          className="cart-item"
-          name={ title }
-          id={ id }
-          data-testid="product"
-        >
+        <div onClick={ this.handleClick }>
           <div
-            className="title-item"
+            className="cart-item"
+            name={ title }
+            id={ id }
+            data-testid="product"
           >
-            {title}
+            <div
+              className="title-item"
+            >
+              {title}
 
+            </div>
+            <img
+              src={ thumbnail }
+              className="img-item"
+              alt={ title }
+            />
+            <p
+              className="price-item"
+            >
+              {`R$ ${price.toFixed(2)}`}
+
+            </p>
           </div>
-          <img
-            src={ thumbnail }
-            className="img-item"
-            alt={ title }
-          />
-          <p
-            className="price-item"
-          >
-            {`R$ ${price.toFixed(2)}`}
-
-          </p>
         </div>
       </Link>
     );
--- src/pages/ItemPage.jsx
+++ src/pages/ItemPage.jsx
@@ -1,24 +1,46 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { getProductId } from '../services/api';
 
 export default class ItemPage extends Component {
   constructor() {
     super();
-
     this.state = {
-      nomeDoProduto: '',
+      productId: {},
     };
   }
 
-  // handleProductDetails = async () => {
-  //   const results = ;
-
-  // }
-
-  render() {
-    return (
-      <div>
-        aa
-      </div>
-    );
+  componentDidMount() {
+    const { match: { params: { id } } } = this.props;
+    this.getItem(id);
   }
+
+   getItem = async (id) => {
+     const data = await getProductId(id);
+     this.setState({ productId: data });
+   }
+
+   render() {
+     const { productId } = this.state;
+     console.log(productId);
+     return (
+       <div>
+         <h3 data-testid="product-detail-name">{ productId.title }</h3>
+         <img
+           alt="Imagem produto"
+           src={ productId.thumbnail }
+         />
+         <p>{productId.price }</p>
+       </div>
+     );
+   }
 }
+
+ItemPage.propTypes = {
+  match: PropTypes.shape(
+    { params: PropTypes.shape(
+      { id: PropTypes.number.isRequired },
+    ).isRequired,
+    }.isRequired,
+  ).isRequired,
+};
